f8a17e37419a221adb4051b51b55ee55f75aaf1a
Forhadul Islam <forhadn@yahoo.com>
2016-06-03 08:11:19 +0600

    addclass

---
+++ add.java
@@ -0,0 +1,23 @@
+
+public class add {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+int res=sum(4,5);
+System.out.println(res);
+
+
+	}
+
+	static int sum(int i, int j) {
+		int result=i+j;
+		return result;
+		
+		
+	}
+	
+}
