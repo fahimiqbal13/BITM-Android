66ad7ec7bc951fafafcdcf78c5d4521679c94750
Forhadul Islam <forhadn@yahoo.com>
2016-06-03 08:20:34 +0600

    intra

---
+++ Intro.java
@@ -0,0 +1,33 @@
+/*import java.util.Scanner;
+
+
+public class Intro {
+	
+	public static void main(String[] args) {
+		String pass = "Hello";
+		String getPass;
+		
+		System.out.println("Please enter a password: ");
+		
+		Scanner input = new Scanner(System.in);
+		getPass = input.next();
+		
+		if(getPass.equals(pass)) {
+			System.out.println("Password is correct!");
+		}
+		else {
+			System.out.println("Incorrect!");
+		}
+		
+		input.close();
+		
+		
+		
+		
+		
+}
+}
+*/				
+
+
+
