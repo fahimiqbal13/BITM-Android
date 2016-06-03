9fb81b4b29e6ccd4411fccde5a05baa30e0f7a2d
Forhadul Islam <forhadn@yahoo.com>
2016-06-03 08:20:08 +0600

    gradesystem

---
+++ GradeSystem.java
@@ -0,0 +1,33 @@
+/*import java.util.Scanner;
+
+
+public class GradeSystem {
+
+	public static void main(String[] args) {
+		int marks;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter Marks: ");
+		
+		marks = input.nextInt();
+		
+		if ( marks <0 || marks >100){
+			System.out.println("Invalid marks!");
+		}
+		
+		else if (marks >=80 && marks <101) {
+			System.out.println("A+");
+		}
+		else if (marks >=33 && marks < 80) {
+			System.out.println("A");
+		}
+		else {
+			System.out.println("Failed!");
+		}
+		input.close();
+		
+
+	}
+
+}
+*/
